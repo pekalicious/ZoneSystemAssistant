877223ce559474760f5a3975cd741d97b7284886
Panagiotis Peikidis <pekalicious@gmail.com>
2020-12-13 17:43:43 -0500

    Fix accidental import

--- ZoneSystemAssistant/Views/ItemsPage.xaml.cs
+++ ZoneSystemAssistant/Views/ItemsPage.xaml.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Pekalicious.ZoneSystemAssistant.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
