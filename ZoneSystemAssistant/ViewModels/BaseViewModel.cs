c25909442e5638d6003c646a69b50a219413b84b
Panagiotis Peikidis <pekalicious@gmail.com>
2020-12-13 22:19:49 -0500

    Added some validation methods in base view model

--- ZoneSystemAssistant/ViewModels/BaseViewModel.cs
+++ ZoneSystemAssistant/ViewModels/BaseViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 using Xamarin.Forms;
@@ -10,9 +12,13 @@ using ZoneSystemAssistant.Services;
 
 namespace ZoneSystemAssistant.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
     {
         public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        readonly IDictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+        public bool HasErrors => _errors?.Any(x => x.Value?.Any() == true) == true;
 
         bool isBusy = false;
         public bool IsBusy
@@ -37,6 +43,51 @@ namespace ZoneSystemAssistant.ViewModels
 
             backingStore = value;
             onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                return _errors.SelectMany(x => x.Value);
+            }
+
+            if (_errors.ContainsKey(propertyName)
+                && _errors[propertyName].Any())
+            {
+                return _errors[propertyName];
+            }
+
+            return new List<string>();
+        }
+
+        protected void Validate(Func<bool> rule, string error, [CallerMemberName] string propertyName = "")
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) return;
+
+            if (_errors.ContainsKey(propertyName))
+            {
+                _errors.Remove(propertyName);
+            }
+
+            if (rule() == false)
+            {
+                _errors.Add(propertyName, new List<string> { error });
+            }
+
+            OnPropertyChanged(nameof(HasErrors));
+
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
             OnPropertyChanged(propertyName);
             return true;
         }
