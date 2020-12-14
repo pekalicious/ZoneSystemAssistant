f9944568f0e27f2a42a32915321b9cf359e420da
Panagiotis Peikidis <pekalicious@gmail.com>
2020-12-13 22:23:22 -0500

    Removed duplicate method

--- ZoneSystemAssistant/ViewModels/BaseViewModel.cs
+++ ZoneSystemAssistant/ViewModels/BaseViewModel.cs
@@ -82,16 +82,6 @@ namespace ZoneSystemAssistant.ViewModels
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
 
-        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (Object.Equals(storage, value))
-                return false;
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
